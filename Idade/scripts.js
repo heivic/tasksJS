c8de54c5f953defd75756ad9580d7ec5cc7ed8a5
Fábio Santos <116457022+heivic@users.noreply.github.com>
2024-08-03 01:47:27 -0300

    2.0
    Falta reduzir tamanho das fotos

--- Idade/scripts.js
+++ Idade/scripts.js
@@ -8,25 +8,39 @@ function verificar() {
         alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
         var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
+        var idade = ano - Number(fnao.value)
         var genero = ''
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
         if(fsex[0].checked) {
             genero = 'Homem'
+
             if(idade >=0 && idade < 10){
-                //Criança
+                img.setAttribute('src', 'img/childmale.jpg')
             } else if (idade < 21){
-                //Jovem
+                img.setAttribute('src', 'img/jovemmale.jpg')
             } else if (idade < 50){
-                //Adulto
+                img.setAttribute('src', 'img/adultomale.jpg')
             } else {
-                //Idoso
+                img.setAttribute('src', 'img/idosomale.jpg')
             }
+
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+
+            if(idade >=0 && idade < 10){
+                img.setAttribute('src', 'img/childfem.jpg')
+            } else if (idade < 21){
+                img.setAttribute('src', 'img/jovemfem.jpg')
+            } else if (idade < 50){
+                img.setAttribute('src', 'img/adultofem.jpg') /////////////////
+            } else {
+                img.setAttribute('src', 'img/idosafem.jpg') ////////////
+            }
+
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     } 
 }
