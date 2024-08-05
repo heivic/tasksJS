7c0ea12bef6bc7ccae7fc6e472688ec5e06116dd
Fábio Santos <116457022+heivic@users.noreply.github.com>
2024-08-04 22:07:00 -0300

    p <= 0

--- Super Contador/scripts.js
+++ Super Contador/scripts.js
@@ -11,6 +11,10 @@ function contar() {
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0) {
+            alert(`Passo inválido Considerando PASSO ${p} em PASSO 1`)
+            p = 1
+        }
 
         if(i < f ){
             //Contagem crescente
